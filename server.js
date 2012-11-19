86a51bfba04ed9791944b5cdf0e35377d38a74b4
Matt Chadburn <matt.chadburn@guardian.co.uk>
2012-11-19 22:01:12 +0000

    Seperate module from the http server
    Tests
    
    - Should fetch an image from an origin server and resize it
    - Should adjust the quality of JPEGs images
    - Should only process non-HTTP 200 response codes from the origin server

--- server.js
+++ server.js
@@ -2,54 +2,23 @@
 var im = require('imagemagick'),
     http = require('http'),
     querystring = require('querystring'),
-    url = require('url');
-
-function Proxy(path, res, width, quality) {
-    
-    var data = '',
-        options = {
-            host: 'static.guim.co.uk',
-            port: 80,
-            path: path,
-            method: 'GET'
-            }
-
-    http.get(options, function(response) {
-        
-        response.setEncoding('binary');
-        
-        response.on('data', function(chunk) {
-            data += chunk;
-        });
-        
-        response.on('end', function () {
-            
-            im.resize({
-                srcData: data,
-                width: width,
-                quality: quality,
-                sharpening: 0.2
-            }, function(err, stdout, stderr) {
-                if (err) throw err;
-                res.write(stdout, 'binary');
-                res.end();
-                });
-        })
-
-    });
-}
+    url = require('url'),
+    firecrest = require('./lib/firecrest');
 
 http.createServer(function (req, res) {
+    
     var u = url.parse(req.url, true),
         w = u.query.width || 100,
         q = u.query.quality || 0.5;
-    
-    if (u.pathname === '/favicon.ico') return false;
 
-    res.writeHead(200, {'Content-Type': 'image/jpeg'});
-    Proxy(u.pathname, res, w, q)
+    firecrest.get(u.pathname, w, q, function(stdout) {
+        res.writeHead(200, {'Cache-Control': 'max-age=10'});
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        res.writeHead(200, {'X-I-Bytes': stdout.length});
+        res.write(stdout, 'binary');
+        //res.write(new Buffer(stdout).toString('base64'));
+        res.end();
+    })
 
 }).listen(1337, '127.0.0.1');
-
-
 
---
+++ specs/firecrest.js
@@ -0,0 +1,103 @@
+
+var firecrest = require('../lib/firecrest'),
+    nock = require('nock');
+
+describe('Firecrest', function(){
+  
+  it('Should fetch an image from an origin server and resize it', function(){
+      
+      var bytes,
+          done = false,
+          jpg = nock('http://static.guim.co.uk')
+            .get('/firecrest.jpg')
+            .replyWithFile(200, __dirname + '/fixtures/firecrest.jpg', {
+                'Content-Type': 'image/jpeg'
+            });
+
+      var bytes,
+          done = false;
+      
+      var options = {
+          host: 'static.guim.co.uk',
+          path: '/firecrest.jpg',
+          port: 80,
+          width: 1
+      }
+
+      firecrest.get(options, function(src) {
+        bytes = src.length;
+        done = true;
+        })
+    
+      waitsFor(function(){
+        return done;
+        }, "image not loaded",  100)
+  
+      runs(function() {
+        expect(bytes).toBe(285);
+      }) 
+
+   });
+
+  it('Should adjust the quality of JPEGs images', function() {
+      
+      var bytes,
+          done = false,
+          jpg = nock('http://static.guim.co.uk')
+            .get('/firecrest.jpg')
+            .replyWithFile(200, __dirname + '/fixtures/firecrest.jpg', {
+                'Content-Type': 'image/jpeg'
+            });
+      
+      var options = {
+          host: 'static.guim.co.uk',
+          path: '/firecrest.jpg',
+          width: 2,
+          quality: 0.8
+      }
+
+      firecrest.get(options, function(src) {
+        bytes = src.length;
+        done = true;
+        })
+    
+      waitsFor(function(){
+        return done;
+        }, "image not loaded",  100)
+  
+      runs(function() {
+        expect(bytes).toBe(295);
+      }) 
+
+  });
+
+  it('Should only process non-HTTP 200 response codes from the origin server', function() {
+      
+      var bytes,
+          errorMessage,
+          done = false,
+          fourOhFour = nock('http://static.guim.co.uk')
+            .get('/not-here')
+            .reply(404);
+ 
+      var options = {
+          host: 'static.guim.co.uk',
+          path: '/not-here',
+      }
+
+      firecrest.get(options, function(src) {
+        }, function(err){
+            errorMessage = err.message;
+            done = true;
+        })
+
+      waitsFor(function(){
+        return done;
+        }, "error callback not invoked",  100)
+  
+      runs(function() {
+        expect(errorMessage).toBe('response code was 404');
+      }) 
+  });
+
+});
